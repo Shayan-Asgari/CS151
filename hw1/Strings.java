dfb79a69dc30c2955ea3fa88e576c956ec09ba91
persianboy23 <31261309+persianboy23@users.noreply.github.com>
2019-01-28 19:41:46 -0800

    Updated Strings.java to not use HashMap just in case we could not use data structures as codecheck did not specify to

--- hw1/Strings.java
+++ hw1/Strings.java
@@ -7,16 +7,23 @@ public class Strings
 	public static String uniqueLetters(String str)
 	{
 		StringBuilder sb = new StringBuilder();
-		HashMap<Character,Integer> uniqueLetters = new HashMap<>();
+		//Comparing one character to rest of characters
 		for(int i = 0; i<str.length(); i++)
 		{
-			uniqueLetters.put(str.charAt(i), uniqueLetters.getOrDefault(str.charAt(i),0) + 1);
-		}
-		for(int i = 0; i<str.length(); i++)
-		{
-			if(uniqueLetters.get(str.charAt(i)) == 1)
-				sb.append(str.charAt(i));
+			String first = str.substring(i, i+1);
+			for(int j = 0; j<str.length(); j++)
+			{
+				String second = str.substring(j,j+1);
+				if(first.equals(second) && i!=j)
+					break;
+				else if(j==str.length()-1)
+					sb.append(first);
+			}
 		}
 		return sb.toString();
+	}
+	public static void main(String[] args)
+	{
+		System.out.println(Strings.uniqueLetters("oorinn"));
 	}
 }
